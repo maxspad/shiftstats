18380bee63feee4fd5df01bbdbfd00a68ae33100
Maxwell Spadafore <maxspad@umich.edu>
2023-01-29 15:16:14 -0500

    updated data file urls

--- src/config.py
+++ src/config.py
@@ -1,2 +1,2 @@
-BLOCK_DATES_FN = '../data/block_dates_2023.csv'
-RESIDENTS_FN = '../data/residents_2022_2023.csv'
+BLOCK_DATES_FN = 'data/block_dates_2023.csv'
+RESIDENTS_FN = 'data/residents_2022_2023.csv'
